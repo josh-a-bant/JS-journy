7b77e174f77a2ddccaf0da3604f756f52c5b9a1e
josh-a-bant <jashobantbesra@gmail.com>
2025-09-20 22:15:24 +0530

    add new folder

---
+++ BMI - Calculator/js.js
@@ -0,0 +1,8 @@
+let height = parseInt(prompt("enter your height"));
+let weight = parseInt(prompt("enter your weight"));
+let gender = prompt("enter your gender");
+let bmiCatagory;
+
+let firstName = prompt("enter your first name");
+let lastName = prompt("enter your last name");
+let middleName = null;
