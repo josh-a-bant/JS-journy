3acee9464ccab53f373aea499092d645987ba759
josh-a-bant <jashobantbesra@gmail.com>
2025-11-05 00:22:43 +0530

    this keyword in arrow function

--- Day - 20/js.js
+++ Day - 20/js.js
@@ -1,1 +1,26 @@
+// "use strict";
+
 console.log(this);
+
+// function x() {
+//   console.log(this);
+// }
+// x();
+
+let obj = {
+  a: 10,
+  // y: function () {
+  //   const x = () => {
+  //     console.log(this);
+  //     // console.log(this.a);
+  //   };
+
+  //   x();
+  // },
+
+  z: () => {
+    console.log(this);
+  },
+};
+
+obj.z();
