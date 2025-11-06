a81653e1e7fba9f092ceadfc62dd8b321291e82d
josh-a-bant <jashobantbesra@gmail.com>
2025-11-07 01:34:49 +0530

    example of function expression

--- Day - 22/js.js
+++ Day - 22/js.js
@@ -1,1 +1,18 @@
-console.log("hi");
+// const helloJK = function () {
+//   console.log("JK");
+// };
+
+// helloJK();
+
+const numbers = [1, 2, 3, 4, 5];
+
+const cubes = numbers.map(function (number) {
+  return Math.pow(number, 3);
+});
+
+const square = numbers.map(function (number) {
+  return Math.pow(number, 2);
+});
+
+console.log(square);
+console.log(cubes);
