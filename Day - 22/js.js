69a4a274ff288ef746199a6ea23d6f6e171a03e1
josh-a-bant <jashobantbesra@gmail.com>
2025-11-07 01:49:42 +0530

    simple example on arrow function

--- Day - 22/js.js
+++ Day - 22/js.js
@@ -4,17 +4,32 @@
 
 // helloJK();
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-const cubes = numbers.map(function (number) {
-  return Math.pow(number, 3);
-});
+// const cubes = numbers.map(function (number) {
+//   return Math.pow(number, 3);
+// });
 
-const square = numbers.map(function (number) {
-  return Math.pow(number, 2);
-});
+// const square = numbers.map(function (number) {
+//   return Math.pow(number, 2);
+// });
 
-console.log(square);
-console.log(cubes);
+// console.log(square);
+// console.log(cubes);
 
 // function expressions = a way to define functions as values or variables
+
+const hello = (name) => {
+  console.log(`Hello ${name}`);
+};
+hello("JK");
+
+const numbers = [1, 2, 3, 4, 5];
+const square = numbers.map((number) => Math.pow(number, 2));
+const cubes = numbers.map((number) => Math.pow(number, 3));
+const total = numbers.reduce((acc, number) => acc + number);
+console.log(square);
+console.log(cubes);
+console.log(total);
+
+//  arrow functions = a concise way to write function expressions good for simple functions that you use only once
