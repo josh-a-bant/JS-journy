f52d857b799ae61ca4cc63b7d2134e6b195b68b1
josh-a-bant <jashobantbesra@gmail.com>
2025-11-08 01:37:38 +0530

    add notes about getter and setter methods

---
+++ Day - 23/js.js
@@ -0,0 +1,6 @@
+//getter = special method that makes a property readable
+// setter = special method that makes a property writeable
+
+// validate and modify a value when reading/writing a property
+
+console.log("JK");
