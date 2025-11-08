37477f4b8fe7b102efad4d92b9ba10660bd853d2
josh-a-bant <jashobantbesra@gmail.com>
2025-11-09 02:37:42 +0530

    add notes about synchronous and asynchronous

--- Day - 24/js.js
+++ Day - 24/js.js
@@ -24,3 +24,7 @@ counter.increment();
 
 // closure = A function defined inside of another function, the inner function has access to the variables and scope of the outer function.
 // Allow for private variables and state maintenance Used frequently in JS frameworks: React, Vue, Angular
+
+// synchronous  = Executes line by line consecutively in a sequential manner Code that waits for an operation to complete.
+
+// asynchronous = Allows multiple operations to be performed concurrently without waiting. Doesn't block the execution flow and allows the program to continue. (I/O operations, network requests, fetching data) Handled with: Callbacks, Promises, Async/Await
