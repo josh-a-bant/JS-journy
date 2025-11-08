0a578acb5e727a38758d3f6ecac4d7b65943be2d
josh-a-bant <jashobantbesra@gmail.com>
2025-11-09 00:57:50 +0530

    notes for closure

--- Day - 24/js.js
+++ Day - 24/js.js
@@ -16,4 +16,11 @@ counter.increment();
 counter.increment();
 counter.increment();
 
+counter.count = 0;
+
 console.log(counter.count);
+
+counter.increment();
+
+// closure = A function defined inside of another function, the inner function has access to the variables and scope of the outer function.
+// Allow for private variables and state maintenance Used frequently in JS frameworks: React, Vue, Angular
