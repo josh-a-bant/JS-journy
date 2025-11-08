6bdfa3a8f58c221b1c82bebcf08d7c0aa55d75eb
josh-a-bant <jashobantbesra@gmail.com>
2025-11-08 20:21:20 +0530

    sort method in js

--- Day - 23/js.js
+++ Day - 23/js.js
@@ -3,4 +3,17 @@
 
 // validate and modify a value when reading/writing a property
 
-console.log("JK");
+// console.log("JK");
+// const obj = {
+//   a: 1,
+//   b: 2,
+// };
+
+// // console.log(obj.toString());
+// console.log(c);
+// c = 100;
+// console.log(c);
+
+const numbers = [1, 10, 2, 5, 6, 7, 5, 9, 4, 3, 9];
+numbers.sort();
+console.log(numbers);
