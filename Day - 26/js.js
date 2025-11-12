cca68d1d80ed59b5be0d3d764853f09f06f74c93
josh-a-bant <jashobantbesra@gmail.com>
2025-11-12 23:43:22 +0530

    JSON

--- Day - 26/js.js
+++ Day - 26/js.js
@@ -1,2 +1,25 @@
-const array = [1, 2, 3];
-console.log(array.length);
+// const array = [1, 2, 3];
+// console.log(array.length);
+
+const sample = [
+  {
+    id: 1,
+    name: "Jyoti Besra",
+    email: "jb@example.com",
+    age: 25,
+    city: "Barpali",
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: "Jashobanta Besra",
+    email: "jb@example.com",
+    age: 25,
+    city: "Padampur",
+    isActive: false,
+  },
+];
+
+const parseToString = JSON.stringify(sample);
+console.log(sample);
+console.log(parseToString);
