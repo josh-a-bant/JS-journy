b3a660691a73c1209094600c0e66ea635fc60bf0
josh-a-bant <jashobantbesra@gmail.com>
2025-11-12 23:56:22 +0530

    add notes

--- Day - 26/js.js
+++ Day - 26/js.js
@@ -44,3 +44,9 @@ const sampleString = `[
 
 const parseToJson = JSON.parse(sampleString);
 console.log(parseToJson);
+
+// JSON = (JavaScript Object Notation) data-interchange format Used for exchanging data between a server and a web application
+// JSON files {key:value} OR [value1, value2, value3]
+
+// JSON.stringify() = converts a JS object to a JSON string.
+// JSON.parse() = converts a JSON string to a JS object
