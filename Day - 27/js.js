ace4247f15a4eac4ec3801b64cfaa7bb3ed88610
josh-a-bant <jashobantbesra@gmail.com>
2025-11-13 13:28:11 +0530

    complete these task

--- Day - 27/js.js
+++ Day - 27/js.js
@@ -1,3 +1,25 @@
 // https://pokeapi.co/api/v2/pokemon/pikachu? - API
 
-console.log("hi");
+async function getPokemonDetail() {
+  const pokemonName = document.getElementById("pokemonName").value;
+
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+
+    if (!response.ok) {
+      throw new Error("enter valid name");
+    }
+
+    const data = await response.json();
+    const pokemonImage = document.getElementById("pokemonImage");
+
+    const pokemonimg = data.sprites.front_default;
+
+    pokemonImage.src = pokemonimg;
+    pokemonImage.style.display = "block";
+  } catch (error) {
+    console.error(error);
+  }
+}
